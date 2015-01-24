c921c7530dff7341ff3cdae588e060c3c6d5b2e0
Maxim Salikhov <msalihov@users.noreply.github.com>
2015-01-24 10:47:50 -0500

    Create AutonomousNonPeriodic.java

---
+++ src/com/team5557/code2015/autonomous/programs/AutonomousNonPeriodic.java
@@ -0,0 +1,12 @@
+package com.team5557.code2015.autonomous;
+
+public interface AutonomousNonPeriodic extends Autonomous {
+	
+	public void runNextAction();
+	
+	@Override
+	public default void periodic() {
+		runNextAction();
+	}
+	
+}
