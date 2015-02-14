29c140dbcd37ee093f0581a040dbe6ac03ea2907
Maxim Salikhov <msalihov@gmail.com>
2015-02-13 20:56:47 -0500

    Comments for Utility classes

--- src/com/team5557/code2015/utility/DashboardController.java
+++ src/com/team5557/code2015/utility/DashboardController.java
@@ -4,8 +4,14 @@ import com.team5557.code2015.RobotSensorController;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+/**
+ * Anything related to SmartDashboard output goes here
+ */
 public class DashboardController {
 
+	/**
+	 * Function to be called periodically
+	 */
 	public static void putData() {
 		SmartDashboard.putNumber("Potentiometer Percent", RobotSensorController
 				.getPotentiometer("main").getPercent());
