bd0491b381870949efc16f68c7ad349c559cc80d
Maxim Salikhov <msalihov@gmail.com>
2015-02-13 23:08:34 -0500

    Output speed data from motors onto dashboard

--- src/com/team5557/code2015/utility/DashboardController.java
+++ src/com/team5557/code2015/utility/DashboardController.java
@@ -1,5 +1,6 @@
 package com.team5557.code2015.utility;
 
+import com.team5557.code2015.RobotMotorController;
 import com.team5557.code2015.RobotSensorController;
 import com.team5557.code2015.sensors.SensorBuiltInAccelerometer;
 
@@ -17,12 +18,24 @@ public class DashboardController {
 		// Put main actuator percentage
 		SmartDashboard.putNumber("Potentiometer Percent", RobotSensorController
 				.getPotentiometer("main").getPercent());
+		// Put accelerometer data
 		SmartDashboard.putNumber("Accelerometer X",
 				SensorBuiltInAccelerometer.getX());
 		SmartDashboard.putNumber("Accelerometer Y",
 				SensorBuiltInAccelerometer.getY());
 		SmartDashboard.putNumber("Accelerometer Z",
 				SensorBuiltInAccelerometer.getZ());
+		// Put speed data from all motors
+		SmartDashboard.putNumber("Motor 0", RobotMotorController.getMotor(0)
+				.get());
+		SmartDashboard.putNumber("Motor 1", RobotMotorController.getMotor(1)
+				.get());
+		SmartDashboard.putNumber("Motor 2", RobotMotorController.getMotor(2)
+				.get());
+		SmartDashboard.putNumber("Motor 3", RobotMotorController.getMotor(3)
+				.get());
+		SmartDashboard.putNumber("Motor 9", RobotMotorController.getMotor(9)
+				.get());
 	}
 
 }
